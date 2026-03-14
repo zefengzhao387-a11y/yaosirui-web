4ca883b6164aad55c38670a1a02b22a6c124e031
zefengzhao387-a11y <zefengzhao387@gmail.com>
2026-03-14 16:28:10 +0800

    Update route.ts

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  return NextResponse.json({
+export function GET() {
+  return Response.json({
     message: "你好，这是后端 API 返回的数据！",
     timestamp: new Date().toISOString(),
   });
